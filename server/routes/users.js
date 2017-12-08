9feaaa79112b196c385156e209e66f96904e8ce4
Zsolt Annár <zsolt921009@gmail.com>
2017-12-08 16:54:57 +0100

    New Dependency [JQUERY, Bootstrap, Popper]

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res, next) {
 				res.json(err)
 			} else {
 				// render to views/user/list.ejs template file
-				res.send(200, rows)
+				res.status(200).send(rows)
 			}
 		})
 	})
