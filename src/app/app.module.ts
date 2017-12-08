5327dc0eda3e365da1520c6ea1915b70a1595e36
Zsolt Annár <zsolt921009@gmail.com>
2017-12-08 17:39:25 +0100

    Add Projects & Main-menu services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './/app-routing.module';
 import { ProjectsComponent } from './projects/projects.component';
 import { HomeComponent } from './home/home.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ProjectsService } from './projects.service';
+import { MainMenuService } from './main-menu.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,7 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [ProjectsService, MainMenuService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
